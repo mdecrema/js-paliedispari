fdf47da506b1459147d73ca1f80f52be2daf45c2
marco <marcodecrema@libero.it>
2020-08-27 16:00:00 +0200

    pari o dispari

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ document.getElementById("pali").addEventListener("click", function() {
     prova2 += parola.charAt(a);
     console.log(prova+" "+prova2);
 });
+
+
+document.getElementById("dispari").addEventListener("click", function() {
+  var answer= prompt("PARI(p) o DISPARI(d)?");
+  var check = false;
+  var numero= parseInt(prompt("Inserisci un NUMERO da 1 a 5:"));
+  var numRandom = Math.floor(Math.random() *5)+1;
+  console.log(numRandom);
+  var somma= numero + numRandom;
+  if (somma % 2 == 0 && answer == "p") {
+    alert("Hai VINTO");
+  } else if ( somma % 2 != 0 && answer == "d") {
+    alert("Hai VINTO");
+  } else {
+    alert("LOSER");
+  }
+});
