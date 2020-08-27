ddfc0c6b9fd812a068fae0e70e5f485fc87c4bb2
marco <marcodecrema@libero.it>
2020-08-27 16:04:53 +0200

    commenti

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 
-
+// PAROLA PALINDROMA
 document.getElementById("pali").addEventListener("click", function() {
+  // Richiesta di una parola all'utente
   var parola= prompt("Inserisci una parola:");
+  // Variabili di supporto
   var prova= "";
   var prova2= "";
+  // Metodo per ottenere il numero di lettere nella parola
   var numLettere= parola.length;
+  // Metà del totale di lettere presenti
   var halfWord= numLettere/2;
+  // PROVA PROVA PROVA PROVA
   for (var i=0; i<halfWord; i++) {
    prova += parola.charAt(i);
   }
@@ -15,12 +20,16 @@ document.getElementById("pali").addEventListener("click", function() {
 });
 
 
+// PARI o DISPARI
 document.getElementById("dispari").addEventListener("click", function() {
+  // Richiesta "pari o dispari" all'utente
   var answer= prompt("PARI(p) o DISPARI(d)?");
-  var check = false;
+  // Richiesta numero da 1 a 5 all'utente
   var numero= parseInt(prompt("Inserisci un NUMERO da 1 a 5:"));
+  // Generatore di numero random da 1 a 5
   var numRandom = Math.floor(Math.random() *5)+1;
   console.log(numRandom);
+  // Condizioni
   var somma= numero + numRandom;
   if (somma % 2 == 0 && answer == "p") {
     alert("Hai VINTO");
